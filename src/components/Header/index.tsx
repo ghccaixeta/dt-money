19363d8fd035334a2572a5a54fd03c85af08a05a
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2024-03-23 18:03:02 -0300

    Commit: Estrutura visual - Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,31 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/icons/Logo.svg'
 import { Plus } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="" />
-                <NewTransactionButton>
-                    <Plus size={20} />
-                    Nova transação
-                </NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+
+                        <NewTransactionButton>
+                            <Plus size={20} />
+                            Nova transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
